725fe15be54786ffcb757b2e46663466caef33b1
riquew <hwindlin@gmail.com>
2024-01-23 17:46:54 -0300

    Estiliza Form e inicia Api

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Input from "../Form/Input";
+import Button from "../Form/Button";
+import useForm from "../../Hooks/useForm";
+import { UserContext } from "../../UserContext";
 
 const LoginForm = () => {
+  const username = useForm();
+  const password = useForm();
+
+  const { userLogin } = React.useContext(UserContext);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (username.validate() && password.validate()) {
+      userLogin(username.value, password.value);
+    }
+  }
+
   return (
-    <div>
-      LoginForm
+    <section>
+      <h1>Login</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <Input label="Usuario" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Entrar</Button>
+      </form>
       <Link to="/login/criar">Cadastro</Link>
-    </div>
+    </section>
   );
 };
 
